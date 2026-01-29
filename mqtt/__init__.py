689742683dd202bcc438eacbae9197352d902d5f
zora <zora@aotudeMacBook-Pro.local>
2026-01-29 16:13:42 +0800

    更新代码

--- mqtt/__init__.py
+++ mqtt/__init__.py
@@ -6,7 +6,7 @@ OneNET MQTT 模块
 提供 Token 生成和设备配置功能
 """
 
-from mqtt import onenet_token
-from mqtt import config
+from . import onenet_token
+from . import config
 
 __all__ = ["onenet_token", "config"]
